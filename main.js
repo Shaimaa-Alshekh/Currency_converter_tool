53d832f55e69a4d28ca6b450e96d8d33c204b391
Shaimaa Alshekh <shaimaa30mazen99@gmail.com>
2024-01-15 17:28:30 +0200

    currency convert tool

---
+++ main.js
@@ -0,0 +1,26 @@
+var amount=document.querySelector("input");
+var ex=document.querySelector("select");
+var res=document.querySelector(".result");
+var convertForm=document.querySelector(".convertform");
+
+convertForm.onsubmit=function(e){
+    e.preventDefault();
+
+    if(ex.selectedIndex == 0){
+        res=( amount.value ) *  3.7771;
+
+    }else if(ex.selectedIndex == 1){
+        res=( amount.value ) * 5.3274;
+
+    }else{
+        res= amount.value ;
+    }
+
+
+console.log(amount.value);
+console.log(ex.selectedIndex);
+console.log(res);
+document.querySelector(".result").innerHTML=res; 
+   
+
+}
